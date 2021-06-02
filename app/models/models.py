1bfeb2a94b7a680d98d8fb36330d71b3b100c427
Zane Preudhomme <zane@effarig.com>
2021-06-02 11:04:45 -0700

    Backend tables pretty well setup

--- app/models/models.py
+++ app/models/models.py
@@ -135,6 +135,8 @@ class Route(db.Model):
 
 
 class State(db.Model):
+    __tablename__ = "states"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     abbr = db.Column(db.String, nullable=False)
