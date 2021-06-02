055bef42925d766489ec755e06d5791fc0e457c5
Zane Preudhomme <zane@effarig.com>
2021-06-02 10:16:55 -0700

    Pretty sure all the database relationships are setup

--- app/models/models.py
+++ app/models/models.py
@@ -15,6 +15,18 @@ class User(db.Model, UserMixin):
     profile_photo = db.Column(db.String(), default='https://www.whittierfirstday.org/wp-content/uploads/default-user-image-e1501670968910.png')  # noqa
     created_at = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)  # noqa
 
+    hikes_owned = db.relationship(
+        "Hike",
+        back_populates="owner",
+        cascade='all, delete-orphan'
+    )
+
+    routes_owned = db.relationship(
+        "Route",
+        back_populates="owner",
+        cascade='all, delete-orphan'
+    )
+
     @property
     def password(self):
         return self.hashed_password
@@ -31,7 +43,17 @@ class User(db.Model, UserMixin):
             "id": self.id,
             "username": self.username,
             "full_name": self.full_name,
-            "profile_photo": self.profile_photo
+            "profile_photo": self.profile_photo,
+            "created_at": str(self.created_at),
+            "hikes": self.hikes_owned.to_dict_basic(),
+            "routes": self.routes_owned.to_dict_basic()
+            }
+
+    def to_dict_basic(self):
+        return {
+            "id": self.id,
+            "full_name": self.full_name,
+            "profile_photo": self.profile_photo,
             }
 
 
@@ -42,9 +64,39 @@ class Hike(db.Model):
     name = db.Column(db.String, nullable=False)
     location = db.Column(db.String, nullable=False)
     desription = db.Column(db.Text)
-    user_id = db.Column(db.Integer, nullable=False)
-    state_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    state_id = db.Column(db.Integer, db.ForeignKey("states.id"), nullable=False)  # noqa
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)  # noqa
+
+    owner = db.relationship(
+        "User",
+        back_populates="hikes_owned"
+    )
+
+    state = db.relationship(
+        "State",
+        back_populates="hikes_in_state"
+    )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "location": self.location,
+            "description": self.desription,
+            "created_at": str(self.created_at),
+            "owner": owner.to_dict_basic(),
+            "state": state.to_dict_basic()
+        }
+
+    def to_dict_basic(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "location": self.location,
+            "description": self.desription,
+            "created_at": str(self.created_at),
+        }
 
 
 class Route(db.Model):
@@ -54,10 +106,55 @@ class Route(db.Model):
     name = db.Column(db.String, nullable=False)
     map_info = db.Column(db.String, nullable=False)
     description = db.Column(db.Text)
-    user_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)  # noqa
+
+    owner = db.relationship(
+        "User",
+        back_populates="routes_owned"
+    )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "map_info": self.map_info,
+            "description": self.desription,
+            "created_at": str(self.created_at),
+            "owner": owner.to_dict_basic(),
+        }
+
+    def to_dict_basic(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "map_info": self.map_info,
+            "description": self.desription,
+            "created_at": str(self.created_at),
+        }
 
 
 class State(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     abbr = db.Column(db.String, nullable=False)
+
+    hikes_in_state = db.relationship(
+        "Hike",
+        back_populates="state"
+    )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "abbr": self.abbr,
+            "hikes": self.hikes_in_state.to_dict_basic()
+        }
+
+    def to_dict_basic(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "abbr": self.abbr,
+        }
