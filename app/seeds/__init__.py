2887d285a492dce7d607fe27eb9ad7675af35592
Zane Preudhomme <zane@effarig.com>
2021-06-08 08:43:49 -0700

    Iniital seed files for hike down

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .states import seed_states, undo_states
+from .hikes import seed_hikes, undo_hikes
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -12,19 +13,23 @@ seed_commands = AppGroup('seed')
 def seed():
     seed_users()
     seed_states()
+    seed_hikes()
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
+    undo_hikes()
     undo_states()
+    undo_users()
 
 
-# Creates the `flask seed all` command
-@seed_commands.command('undo')
+# Creates the `flask seed run` command
+@seed_commands.command('run')
 def all():
-    undo_users()
+    undo_hikes()
     undo_states()
+    undo_users()
     seed_users()
     seed_states()
+    seed_hikes()
--- app/seeds/hikes.py
+++ app/seeds/hikes.py
@@ -1,10 +1,35 @@
 from faker import Faker
+import random
 from app.models import db, Hike
 
 
 def seed_hikes():
     fake = Faker()
 
-    # hikes= [
-    #     'name': 'Grand Canyon', 'latitude':'','longitude':'','description':'','user_id':'','state_id':''}  # noqa
-    # ]
+    hikes = [
+        {'name': 'Grand Canyon', 'latitude': 36.05798, 'longitude': -112.1267, 'description': "It's a long hike through the deepest canyon in the world.", 'user_id': 1, 'state_id': 3}  # noqa
+    ]
+
+    for i in range(50):
+        info = fake.local_latlng()
+        hikes.append({
+            'name': info[2],
+            'latitude': info[0],
+            'longitude': info[1],
+            'description': fake.sentence(nb_words=random.randint(1, 15)),
+            'user_id': 1,
+            'state_id': 2
+        })
+
+    for hike in hikes:
+        print(hike)
+        load_hike = Hike(name=hike["name"], latitude=hike["latitude"], longitude=hike["longitude"], description=hike["description"], user_id=hike["user_id"], state_id=hike["state_id"])  # noqa
+        
+        db.session.add(load_hike)
+
+    db.session.commit()
+
+
+def undo_hikes():
+    db.session.execute('TRUNCATE hikes RESTART IDENTITY CASCADE;')
+    db.session.commit()
