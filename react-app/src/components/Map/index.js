63acdf9266342400dd1cd47e642da1e7e1ee51f1
Zane Preudhomme <zane@effarig.com>
2021-06-08 13:00:10 -0700

    InfoWindow now working on the map

--- react-app/src/components/Map/index.js
+++ react-app/src/components/Map/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux'
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
 require('dotenv').config()
 
@@ -12,9 +12,31 @@ const mapStyles = {
   display: 'inline-block'
 };
 
+const containerStyle = {
+  position: 'relative',
+  width: '1000px',
+  height: '750px',
+  top: '25px',
+  margin: '0 auto'
+}
+
 
 const MapContainer = (props) => {
     const userLocation = useSelector(state => state.location.userLocation)
+    const hikes = useSelector(state => state.hike)
+
+    const [selectedHike, setSelectedHike] = useState();
+
+    const markerClick = (hike) => {
+      setSelectedHike(hike)
+      console.log("I have set selected hike to:   ", hike)
+    }
+
+    // useEffect(() => {
+    //   console.log(selectedHike)
+    // }, [hikes, selectedHike])
+
+
     let center = { lat: 35.3462, lng: -111.6791 }
     if (userLocation){
       center = {lat: userLocation.latitude, lng: userLocation.longitude}
@@ -23,20 +45,46 @@ const MapContainer = (props) => {
       center = props.center
     }
 
+    let markers = (hikes.hikes.map((place, i)=> (
+      <Marker 
+        key={i}
+        position={{lat: place.latitude, lng: place.longitude}}
+        onClick={() => markerClick(place)}
+      />
+    )));
+
+    // let markerWindows = 
+
     return (
-      <Map
-        google={props.google}
-        zoom={12}
-        style={mapStyles}
-        initialCenter={center}
-      >
-        <Marker key="test"
-        position={{lat: 35.3462, lng: -111.6791}}
-        />
-      </Map>
+      <div className="map_wrapper">
+      {hikes !== null && 
+        <Map
+          google={props.google}
+          zoom={8}
+          containerStyle={containerStyle}
+          initialCenter={center}
+        >
+          { markers }
+          {selectedHike &&
+            <InfoWindow 
+            position ={{
+              lat: selectedHike.latitude,
+              lng: selectedHike.longitude
+            }}
+            visible={true}
+            >
+              <div>
+                <h1>{selectedHike.name}</h1>
+                <p>{selectedHike.description}</p>
+              </div>
+            </InfoWindow>
+          }
+        </Map>
+      }
+      </div>
     );
   
 }
 export default GoogleApiWrapper({
-  // apiKey: API_KEY
+  apiKey: API_KEY
 })(MapContainer);
